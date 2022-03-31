4c5d7352fb4f5b53b048644029a3bc07bdc1dff2
Chris DeLashmutt <cdelashmutt@pivotal.io>
2022-03-31 14:57:04 -0400

    Make HARBOR_USER optional

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -2,5 +2,6 @@
 set -x
 set +e
 
-REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user ${HARBOR_USER}
+REGISTRY_USER=${HARBOR_USER:-admin}
+REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user $REGISTRY_USER
 
