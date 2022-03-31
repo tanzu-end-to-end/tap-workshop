d3db7e6a9ecbfe9a8885a357f2345f869e4efa5b
Chris DeLashmutt <cdelashmutt@pivotal.io>
2022-03-31 14:29:43 -0400

    Add in ability to specify harbor user

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -2,5 +2,5 @@
 set -x
 set +e
 
-REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user admin
+REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user ${HARBOR_USER}
 
