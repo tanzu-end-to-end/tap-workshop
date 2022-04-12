35730975d5584d3c772a1a6a8689c9e777d586de
cpage-pivotal <cpage@pivotal.io>
2022-04-12 11:50:31 -0500

    Update workshop image

--- workshop/setup.d/03-path-code-server-config.sh
+++ workshop/setup.d/03-path-code-server-config.sh
@@ -12,11 +12,10 @@ set -eo pipefail
 CODE_SERVER_BIND_ADDRESS=${CODE_SERVER_BIND_ADDRESS:-127.0.0.1}
 
 #EDITOR_HOME=${EDITOR_HOME:-/home/eduk8s/spring-sensors}
-EDITOR_HOME=/home/eduk8s/spring-sensors
 
 exec /opt/code-server/bin/code-server \
     --bind-addr "$CODE_SERVER_BIND_ADDRESS:10085" \
     --auth none \
     --disable-telemetry \
-    $EDITOR_HOME
+    /home/eduk8s/spring-sensors
 EOF
