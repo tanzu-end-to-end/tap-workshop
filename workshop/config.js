a9ae2f9ee85f78ab89227d6c3e99a27287ad3009
cpage-pivotal <cpage@pivotal.io>
2022-04-18 15:58:28 -0500

    Testing content

---
+++ workshop/config.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const { spawnSync } = require('child_process');
+
+function initialize(workshop) {
+    workshop.load_workshop();
+
+    let fileContents = fs.readFileSync('/home/eduk8s/.kube/config');
+    let data = yaml.safeLoad(fileContents);
+
+    workshop.data_variable('user_token', data.users[0].user.token);
+}
+
+exports.default = initialize;
+
+module.exports = exports.default;
