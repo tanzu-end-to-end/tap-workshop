9b17b3faaabeb669640b016d4b441100d9efd3b3
cpage-pivotal <cpage@pivotal.io>
2022-04-26 14:48:30 -0500

    GitOps support

--- workshop/setup.d/01-gitops-repo.sh
+++ workshop/setup.d/01-gitops-repo.sh
@@ -8,8 +8,7 @@ echo $REPO_NAME > repo.txt
 mkdir spring-sensors
 cd spring-sensors
 echo "# Spring Sensors Application" >> README.MD
-git init
-git checkout -b main
+git init -b main
 git config user.name gitea_admin
 git config user.email "gitea_admin@example.com"
 git add .
@@ -17,3 +16,7 @@ git commit -a -m "Initial Commit"
 
 git remote add origin https://gitea_admin:$GITEA_PASSWORD@gitea.${INGRESS_DOMAIN}/gitea_admin/$REPO_NAME.git
 git push -u origin main
+
+cd ..
+git clone https://gitea_admin:$GITEA_PASSWORD@gitea.${INGRESS_DOMAIN}/gitea_admin/gitops-workloads.git
+envsubst < workload.yaml > gitops-workloads/workload-$SESSION_NAMESPACE.yaml
