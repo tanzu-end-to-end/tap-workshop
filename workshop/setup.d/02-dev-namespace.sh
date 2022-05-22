7cea3ca22037c4de376265a71cdd3bddc2da0d2a
cpage-pivotal <cpage@pivotal.io>
2022-05-22 17:38:51 -0500

    Using jq to update code-server settings

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -4,4 +4,3 @@ set +e
 
 REGISTRY_USER=${HARBOR_USER:-admin}
 REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user $REGISTRY_USER
-
