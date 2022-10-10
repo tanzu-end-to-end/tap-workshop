0b24c511d7ebc0251ac317c2dc276dc978219074
Corby <pcorby@vmware.com>
2022-10-10 08:57:49 -0500

    1.3 Config

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -5,4 +5,7 @@ set +e
 REGISTRY_USER=${HARBOR_USER:-admin}
 REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user $REGISTRY_USER
 
+echo $KUBE_CONFIG
+
+echo $KUBE_CONFIG > config
 # kubectl get cm context -n $WORKSHOP_NAMESPACE -o jsonpath='{.data.config}' > .kube/config
