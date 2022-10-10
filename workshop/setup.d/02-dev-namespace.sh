7dad286c1c20c37a4d0e5d7d2374f456b83b1019
Corby <pcorby@vmware.com>
2022-10-10 07:36:32 -0500

    1.3 Config

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -4,3 +4,5 @@ set +e
 
 REGISTRY_USER=${HARBOR_USER:-admin}
 REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user $REGISTRY_USER
+
+kubectl get cm workshop -n admin -o jsonpath='{.data.config}' > config
