80e417771696128359c39ba9dd9ccd4815dd417a
Chris DeLashmutt <cdelashmutt@vmware.com>
2023-05-07 15:36:36 -0400

    Enable other registry urls

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -3,4 +3,5 @@ set -x
 set +e
 
 REGISTRY_USER=${HARBOR_USER:-admin}
-REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user $REGISTRY_USER
+REGISTRY_URL=${REGISTRY_URL:-harbor.${INGRESS_DOMAIN}}
+REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry $REGISTRY_URL --registry-user $REGISTRY_USER
