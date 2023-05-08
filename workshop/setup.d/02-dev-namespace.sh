854852077b52658e23f47672b82f3d074fcbef16
Chris DeLashmutt <cdelashmutt@vmware.com>
2023-05-07 21:49:15 -0400

    Add option to pull in secrets from export

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -2,6 +2,10 @@
 set -x
 set +e
 
-REGISTRY_USER=${HARBOR_USER:-admin}
-REGISTRY_URL=${REGISTRY_URL:-harbor.${INGRESS_DOMAIN}}
-REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry $REGISTRY_URL --registry-user $REGISTRY_USER
+if [ -z ${HARBOR_USER+x} ]; then 
+  kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "exported-creds"}]}'
+else 
+  REGISTRY_USER=${HARBOR_USER:-admin}
+  REGISTRY_URL=${REGISTRY_URL:-harbor.${INGRESS_DOMAIN}}
+  REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry $REGISTRY_URL --registry-user $REGISTRY_USER
+fi
