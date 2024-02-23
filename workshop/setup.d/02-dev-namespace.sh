74c0d73d1c3d8a1c7fa6f51e1e65d7597f7ae47d
Chris DeLashmutt <cdelashmutt@vmware.com>
2024-02-23 10:17:08 -0500

    Use correct creds based on environment

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -3,7 +3,11 @@ set -x
 set +e
 
 if [ -z ${HARBOR_USER+x} ]; then 
-  kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "exported-creds"},{"name": "learningcenter-registry-credentials"}], "secrets": [{"name": "exported-creds"},{"name": "learningcenter-registry-credentials"}]}'
+  embedded_reg_creds="learningcenter-registry-credentials"
+  if [ -z ${POLICY_ENGINE+x} ]; then 
+    embedded_reg_creds="educates-registry-credentials"
+  fi
+  kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "exported-creds"},{"name": "'$embedded_reg_creds'"}], "secrets": [{"name": "exported-creds"},{"name": "'$embedded_reg_creds'"}]}'
 else 
   REGISTRY_USER=${HARBOR_USER:-admin}
   REGISTRY_URL=${REGISTRY_URL:-harbor.${INGRESS_DOMAIN}}
