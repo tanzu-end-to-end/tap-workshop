ef6c5c455d4c0e084a6189b009332e772c8ba5a7
Chris DeLashmutt <cdelashmutt@vmware.com>
2024-02-23 11:25:55 -0500

    Only set if not running in educates

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -3,11 +3,9 @@ set -x
 set +e
 
 if [ -z ${HARBOR_USER+x} ]; then 
-  embedded_reg_creds="learningcenter-registry-credentials"
-  if [[ -n ${POLICY_ENGINE} ]]; then 
-    embedded_reg_creds="educates-registry-credentials"
+  if [[ -z ${POLICY_ENGINE} ]]; then 
+    kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "exported-creds"},{"name": "learningcenter-registry-credentials"}], "secrets": [{"name": "exported-creds"},{"name": "learningcenter-registry-credentials"}]}'
   fi
-  kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "exported-creds"},{"name": "'$embedded_reg_creds'"}], "secrets": [{"name": "exported-creds"},{"name": "'$embedded_reg_creds'"}]}'
 else 
   REGISTRY_USER=${HARBOR_USER:-admin}
   REGISTRY_URL=${REGISTRY_URL:-harbor.${INGRESS_DOMAIN}}
